69eb678a36db7003be1a1ce4cf83bcf7b5528e06
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-15 20:30:48 +0100

    Add project files.

---
+++ ReverseTextExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ReverseTextExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+
+            char[] charArray = userInput.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            string reversedString = new string(charArray);
+
+            Console.WriteLine($"{reversedString}");
+        }
+    }
+}
